16dd2c70ed97ebf635610953d4a2c3fafdf3512a
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-09-21 12:52:32 +0530

    Removed contact and feedback from default forms.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -20,19 +20,3 @@ INSERT INTO `cmg_core_role_permission` VALUES
 	(1,1551),
 	(2,1551),
 	(1551,1551);
-
---
--- Dumping data for table `cmg_form`
---
-
-INSERT INTO `cmg_form` VALUES 
-	(1,NULL,1,1,'contact','contact form','Thanks for contacting us. We will contact you within next 48 hrs.',1,1,0,0,1,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54'),
-	(2,NULL,1,1,'feedback','feedback form','Thanks for providing your valuable feedback.',0,1,1,0,1,NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
-
---
--- Dumping data for table `cmg_form_field`
---
-
-INSERT INTO `cmg_form_field` VALUES 
-	(1,1,'name','Name',1,NULL,NULL),(2,1,'email','Email',1,NULL,NULL),(3,1,'subject','Subject',1,NULL,NULL),(4,1,'message','Message',5,NULL,NULL),
-	(5,2,'name','Name',1,NULL,NULL),(6,2,'email','Email',1,NULL,NULL),(7,2,'rating','Rating',25,NULL,NULL),(8,2,'message','Message',5,NULL,NULL);
