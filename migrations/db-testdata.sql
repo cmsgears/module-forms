9f099df3b8028ec0a05734a5148379c7282a25f1
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-02-18 15:57:40 +0530

    Updated rbac mappings.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -12,9 +12,11 @@ SELECT @roleform := `id` FROM cmg_core_role WHERE slug = 'form-manager';
 INSERT INTO `cmg_core_permission` (`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`icon`,`createdAt`,`modifiedAt`) VALUES 
 	(1,1,'Form','form','system','The permission form is to manage forms from admin.',null,'2014-10-11 14:22:54','2014-10-11 14:22:54');
 
+SELECT @permadmin := `id` FROM cmg_core_permission WHERE slug = 'admin';
+SELECT @permuser := `id` FROM cmg_core_permission WHERE slug = 'user';
 SELECT @permform := `id` FROM cmg_core_permission WHERE slug = 'form';
 
 INSERT INTO `cmg_core_role_permission` VALUES 
 	(@rolesadmin,@permform),
 	(@roleadmin,@permform),
-	(@roleform,@permform);
+	(@roleform,@permadmin),(@roleform,@permuser),(@roleform,@permform);
