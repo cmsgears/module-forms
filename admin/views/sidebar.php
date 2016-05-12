8f1155ca52da00eaffdb9076b1d96bdfb59bf044
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-05-12 18:09:43 +0530

    Updated module name according to project config.

--- admin/views/sidebar.php
+++ admin/views/sidebar.php
@@ -8,7 +8,7 @@ $core	= Yii::$app->cmgCore;
 $user	= Yii::$app->user->getIdentity();
 ?>
 
-<?php if( $core->hasModule( 'cmgforms' ) && $user->isPermitted( 'form' ) ) { ?>
+<?php if( $core->hasModule( 'forms' ) && $user->isPermitted( 'form' ) ) { ?>
 	<div id="sidebar-form" class="collapsible-tab has-children <?php if( strcmp( $parent, 'sidebar-form' ) == 0 ) echo 'active';?>">
 		<div class="collapsible-tab-header clearfix">
 			<div class="colf colf5 wrap-icon"><span class="cmti cmti-checkbox-b-active"></span></div>
@@ -16,9 +16,9 @@ $user	= Yii::$app->user->getIdentity();
 		</div>
 		<div class="collapsible-tab-content clear <?php if( strcmp( $parent, 'sidebar-form' ) == 0 ) echo 'expanded visible';?>">
 			<ul>
-				<li class='form <?php if( strcmp( $child, 'form' ) == 0 ) echo 'active';?>'><?= Html::a( "Forms", ['/cmgforms/form/all'] ) ?></li>
-				<li class='form <?php if( strcmp( $child, 'form-config' ) == 0 ) echo 'active';?>'><?= Html::a( "Config Forms", ['/cmgforms/config/all'] ) ?></li>
-				<li class='form-template <?php if( strcmp( $child, 'form-template' ) == 0 ) echo 'active';?>'><?= Html::a( "Form Templates", ['/cmgforms/form/template/all'] ) ?></li>
+				<li class='form <?php if( strcmp( $child, 'form' ) == 0 ) echo 'active';?>'><?= Html::a( "Forms", ['/forms/form/all'] ) ?></li>
+				<li class='form <?php if( strcmp( $child, 'form-config' ) == 0 ) echo 'active';?>'><?= Html::a( "Config Forms", ['/forms/config/all'] ) ?></li>
+				<li class='form-template <?php if( strcmp( $child, 'form-template' ) == 0 ) echo 'active';?>'><?= Html::a( "Form Templates", ['/forms/form/template/all'] ) ?></li>
 			</ul>
 		</div>
 	</div>
